b396e1e86daac0ccee7108bdf9183a7cbf137fc4
Jan Fridrichovsky <fridrichovsky@gmail.com>
2017-09-29 18:00:43 +0200

    Create the window object and show it

--- OpenGL_Examples/main.cpp
+++ OpenGL_Examples/main.cpp
@@ -5,10 +5,9 @@
 int main()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
@@ -16,6 +15,10 @@ int main()
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
+	}
+	else
+	{
+		std::cout << "The window has been shown correctly" << std::endl;
 	}
 	glfwMakeContextCurrent(window);
 	return 0;
