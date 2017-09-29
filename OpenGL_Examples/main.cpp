88da7344398d84c790cfb7ba2c8cbfde946f0c48
Jan Fridrichovsky <fridrichovsky@gmail.com>
2017-09-29 18:11:14 +0200

    The rendering to the window is done see: https://learnopengl.com/#!Getting-started/Hello-Window

--- OpenGL_Examples/main.cpp
+++ OpenGL_Examples/main.cpp
@@ -2,6 +2,9 @@
 #include <../../glad/include/glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
 int main()
 {
 	glfwInit();
@@ -21,5 +24,44 @@ int main()
 		std::cout << "The window has been shown correctly" << std::endl;
 	}
 	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	else
+	{
+		std::cout << "The GLAD has been initialized correctly" << std::endl;
+	}
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	/* This is the main rendering loop */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Processing of the input */
+		processInput(window);
+
+		/* Renderin commands */
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Get the event and swap buffer */
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
