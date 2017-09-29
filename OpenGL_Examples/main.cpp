bf7d3db1b7fc382e487cc79ed60d6cca483dd776
Jan Fridrichovsky <fridrichovsky@gmail.com>
2017-09-29 20:34:43 +0200

    The triangle is rendered. see: https://learnopengl.com/#!Getting-started/Hello-Triangle

--- OpenGL_Examples/main.cpp
+++ OpenGL_Examples/main.cpp
@@ -2,6 +2,26 @@
 #include <../../glad/include/glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+
+const char* vertexShaderSource = 
+"#version 440 core \n\
+layout(location = 0) in vec3 aPos;\
+void main()\
+{\
+gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\
+}";
+
+const char* fragmentShaderSource = 
+"#version 330 core\n\
+out vec4 FragColor;\
+\
+void main()\
+{\
+	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\
+}";
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
@@ -43,9 +63,73 @@ int main()
 		/* Processing of the input */
 		processInput(window);
 
-		/* Renderin commands */
+		/* Rendering commands */
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Create Vertex Shader */
+		unsigned int vertexShader;
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
+		int  success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
+		/* Create Fragment Shader */
+		unsigned int fragmentShader;
+		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+		glCompileShader(fragmentShader);
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
+		/* Create Shader Program */
+		unsigned int shaderProgram;
+		shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		}
+
+		/* Create Vertex Array */
+		float vertices[] = {
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.0f,  0.5f, 0.0f
+		};
+		unsigned int VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		/* Create Vertex Attribute Object */
+		unsigned int VAO;
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		/* Run the shaders */
+		glUseProgram(shaderProgram);
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
+		/* Draw the rectangle */
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Get the event and swap buffer */
 		glfwPollEvents();
