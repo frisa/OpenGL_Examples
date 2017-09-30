853801081177d33cb78ec3f2fbd52f8f843d4029
Jan Fridrichovsky <fridrichovsky@gmail.com>
2017-09-30 13:54:02 +0200

    The cleaning the code

--- OpenGL_Examples/main.cpp
+++ OpenGL_Examples/main.cpp
@@ -1,45 +1,28 @@
+#include <string>
 #include <iostream>
+#include <fstream>
 #include <../../glad/include/glad/glad.h>
 #include <GLFW/glfw3.h>
 
-
-
-const char* vertexShaderSource = 
-"#version 440 core \n\
-layout(location = 0) in vec3 aPos;\
-void main()\
-{\
-gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\
-}";
-
-const char* fragmentShaderSource = 
-"#version 330 core\n\
-out vec4 FragColor;\
-\
-void main()\
-{\
-	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\
-}";
-
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-void openGLRendering(const unsigned int shaderProgram, const unsigned int VBO, const unsigned int EBO, const unsigned int VAO);
-void openGLPrepare(unsigned int& shaderProgram, unsigned int& VBO, unsigned int& EBO, unsigned int& VAO);
+void openGLRendering(const GLuint shaderProgram, const GLuint VBO, const GLuint EBO, const GLuint VAO);
+void openGLPrepare(GLuint& VBO, GLuint& EBO, GLuint& VAO);
+GLuint uiLoadShadersToProgram(const char* cVertexShaderPath, const char* cFragmentShaderPath, bool bMakeDefault);
 
 int main()
 {
-	unsigned int u32ShaderProgram;
-	unsigned int u32VAO;
-	unsigned int u32EBO;
-	unsigned int u32VBO;
+	GLuint u32VAO;
+	GLuint u32EBO;
+	GLuint u32VBO;
+	GLuint uiShaderProgram;
 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "The OpenGL example", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -63,7 +46,9 @@ int main()
 	}
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	openGLPrepare(u32ShaderProgram, u32VBO, u32EBO, u32VAO);
+
+	uiShaderProgram = uiLoadShadersToProgram("../OpenGL_Examples/shader.vert", "../OpenGL_Examples/shader.frag", false);
+	openGLPrepare(u32VBO, u32EBO, u32VAO);
 	/* This is the main rendering loop */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -71,7 +56,7 @@ int main()
 		processInput(window);
 
 		/* Rendering commands */
-		openGLRendering(u32ShaderProgram, u32VBO, u32EBO, u32VAO);
+		openGLRendering(uiShaderProgram, u32VBO, u32EBO, u32VAO);
 
 		/* Get the event and swap buffer */
 		glfwPollEvents();
@@ -92,7 +77,7 @@ void processInput(GLFWwindow *window)
 		glfwSetWindowShouldClose(window, true);
 }
 
-void openGLRendering(const unsigned int shaderProgram, const unsigned int VBO, const unsigned int EBO, const unsigned int VAO)
+void openGLRendering(const GLuint shaderProgram, const GLuint VBO, const GLuint EBO, const GLuint VAO)
 {
 	/* Clear the window */
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -109,7 +94,7 @@ void openGLRendering(const unsigned int shaderProgram, const unsigned int VBO, c
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
-void openGLPrepare(unsigned int& shaderProgram, unsigned int& VBO, unsigned int& EBO, unsigned int& VAO)
+void openGLPrepare(GLuint& VBO, GLuint& EBO, GLuint& VAO)
 {
 	/* Vertex Array for rectangle */
 	float verticesRectangle[] = {
@@ -124,47 +109,12 @@ void openGLPrepare(unsigned int& shaderProgram, unsigned int& VBO, unsigned int&
 		1, 2, 3    // second triangle
 	};
 
-	unsigned int vertexShader;
-	unsigned int fragmentShader;
+	/* Get the amount of Vertex Attributes supported by hardware */
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
 
-	/* Create Vertex Shader */
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	int  success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	/* Create Fragment Shader */
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	/* Create Shader Program */
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	/* Create Vertex Array for Rectangle */
+	/* Create Vertex Buffer for Rectangle */
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesRectangle), verticesRectangle, GL_STATIC_DRAW);
@@ -179,4 +129,124 @@ void openGLPrepare(unsigned int& shaderProgram, unsigned int& VBO, unsigned int&
 	glBindVertexArray(VAO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+}
+
+GLuint uiLoadShadersToProgram(const char* cVertexShaderPath, const char* cFragmentShaderPath, bool bMakeDefault)
+{
+	GLuint uiVertexShader;
+	GLuint uiFragmentShader;
+
+	std::ifstream vertexShaderFileInBuffer;
+	std::ifstream fragmentShaderFileInBuffer;
+
+	uiVertexShader = glCreateShader(GL_VERTEX_SHADER);
+	uiFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	if (true == bMakeDefault)
+	{
+		std::ofstream shaderOutBuffer;
+
+		std::string vertexShaderFile(R"raw(shader.vert)raw");
+		std::string fragmentShaderFile(R"raw(shader.frag)raw");
+
+		const char* vertexShaderCode = "#version 440 core\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"void main()\n"
+			"{\n"
+			"\tgl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+			"}";
+		const char* fragmentShaderCode = "#version 440 core\n"
+			"out vec4 FragColor;\n"
+			"void main()\n"
+			"{\n"
+			"\tFragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+			"}";
+
+		/* Create default Vertex Shader */
+		shaderOutBuffer = std::ofstream(vertexShaderFile, std::ios::binary);
+		if (!shaderOutBuffer.is_open())
+		{
+			std::cout << "The shader file: " << vertexShaderFile << " can not be created" << std::endl;
+			return 0;
+		}
+		shaderOutBuffer << vertexShaderCode;
+		shaderOutBuffer.close();
+
+		/* Create default Fragment Shader */
+		shaderOutBuffer = std::ofstream(fragmentShaderFile, std::ios::binary);
+		if (!shaderOutBuffer.is_open())
+		{
+			std::cout << "The shader file: " << vertexShaderFile << " can not be created" << std::endl;
+			return 0;
+		}
+		shaderOutBuffer << fragmentShaderCode;
+		shaderOutBuffer.close();
+		/* Get the Shaders Input Buffers for defualt files */
+		vertexShaderFileInBuffer = std::ifstream(cVertexShaderPath, std::ios::binary);
+		fragmentShaderFileInBuffer = std::ifstream(cFragmentShaderPath, std::ios::binary);
+	}
+	else
+	{
+		/* Get the Shaders Input Buffers */
+		vertexShaderFileInBuffer = std::ifstream(cVertexShaderPath, std::ios::binary);
+		fragmentShaderFileInBuffer = std::ifstream(cFragmentShaderPath, std::ios::binary);
+	}
+	std::string sVertexShaderSource((std::istreambuf_iterator<char>(vertexShaderFileInBuffer)), std::istreambuf_iterator<char>());
+	std::string sFragmentShaderSource((std::istreambuf_iterator<char>(fragmentShaderFileInBuffer)), std::istreambuf_iterator<char>());
+	const char* pcVertexShaderSource = sVertexShaderSource.c_str();
+	const char* pcFragmentShaderSource = sFragmentShaderSource.c_str();
+
+	/* Compile Vertex Shader */
+	std::cout << "Compile Vertex Shader: \n" << pcVertexShaderSource << std::endl;
+	glShaderSource(uiVertexShader, 1, &pcVertexShaderSource, NULL);
+	glCompileShader(uiVertexShader);
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(uiVertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(uiVertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cout << "\nCompilation successfull" << std::endl;
+	}
+
+	/* Compile Fragment Shader */
+	std::cout << "Compile Fragment Shader: \n" << pcFragmentShaderSource << std::endl;
+	glShaderSource(uiFragmentShader, 1, &pcFragmentShaderSource, NULL);
+	glCompileShader(uiFragmentShader);
+	glGetShaderiv(uiFragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(uiFragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cout << "\nCompilation successfull" << std::endl;
+	}
+
+	/* Create Shader Program */
+	GLuint uiShaderProgram;
+	uiShaderProgram = glCreateProgram();
+	glAttachShader(uiShaderProgram, uiVertexShader);
+	glAttachShader(uiShaderProgram, uiFragmentShader);
+	glLinkProgram(uiShaderProgram);
+	glGetProgramiv(uiShaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(uiShaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cout << "\nLinking successfull" << std::endl;
+	}
+	glDeleteShader(uiVertexShader);
+	glDeleteShader(uiFragmentShader);
+	return uiShaderProgram;
 }
